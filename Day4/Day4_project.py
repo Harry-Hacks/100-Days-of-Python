bbc4d58bf7b330d273f9a649e50aeaaed4bda22f
Harry Challis <challisjh@gmail.com>
2025-12-02 00:56:55 +0100

    Update Day4_project.py
    Added extra functionality, now prints out user and computer choice in words and describes the win, lose, or tie

--- Day4/Day4_project.py
+++ Day4/Day4_project.py
@@ -31,51 +31,23 @@ scissors = '''
 ---.__(___)
 '''
 
-user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-computer = random.randint(0, 2)
+choice_list = [rock, paper, scissors]
+pick_list = ["Rock", "Paper","Scissors"]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if user >= 3 or user < 0:
-    print("You typed an invalid number, you lose!")
-elif user == 0:
-    print(rock)
-    if computer == 0:
-        print("Computer chose:\n")
-        print(rock)
-        print("It's a draw")
-    elif computer == 1:
-        print(paper)
-        print("Computer chose:\n")
-        print("You lose")
-    else:
-        print(scissors)
-        print("Computer chose:\n")
-        print("You win")
-elif user == 1:
-    print(paper)
-    if computer == 0:
-        print("Computer chose:\n")
-        print(rock)
-        print("You win")
-    elif computer == 1:
-        print("Computer chose:\n")
-        print(paper)
-        print("It's a draw")
-    else:
-        print(scissors)
-        print("Computer chose:\n")
-        print("You lose")
-elif user == 2:
-    print(scissors)
-    if computer == 0:
-        print(rock)
-        print("Computer chose:\n")
-        print("You lose")
-    elif computer == 1:
-        print(paper)
-        print("Computer chose:\n")
-        print("You win")
-    else:
-        print(scissors)
-        print("Computer chose:\n")
-        print("It's a draw")
-        
+if user_choice >= 3 or user_choice < 0:
+    print("Invalid choice, try again!")
+else:
+    print(choice_list[user_choice])
+    print("You chose: " + pick_list[user_choice] + "\n")
+    computer_choice = random.randint(0, 2)
+    print("Computer chose: " + pick_list[computer_choice])
+    print(choice_list[computer_choice])
+    
+if user_choice == computer_choice:
+    print("It's a draw! You both chose " + pick_list[user_choice])
+elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+    print("You win! " + pick_list[user_choice] + " beats " + pick_list[computer_choice])
+else:
+    print("You lose! " + pick_list[computer_choice] + " beats " + pick_list[user_choice])
+    
